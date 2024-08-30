4b83c2aad0a8638fa421c5820c28db7bc0126c46
“iPaulinh0” <pauloaraujo2607@gmail.com>
2024-08-30 05:32:02 -0300

    feat: login page ui

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { PizzaIcon } from 'lucide-react' 
 
 export function AuthLayout() {
   return(
-    <div className="min-h-screen grid-cols-2 ">
-      <div className="h-full border-r border-foreground/5 bg-muted">
+    <div className="grid min-h-screen grid-cols-2 ">
+      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <PizzaIcon className="h-5 w-5"/>
+          <span className="font-semibold">pizza.shop</span>
+        </div>
 
+        <footer className="text-sm">Painel do Parceiro @copy; pizza.shop - {new Date().getFullYear()}</footer>
       </div>
 
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
