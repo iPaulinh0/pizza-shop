3778600cde06c44a97f61810b6100729ae133096
“iPaulinh0” <pauloaraujo2607@gmail.com>
2024-09-25 13:53:40 -0300

    feat: header style configuration

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { PizzaIcon } from 'lucide-react'
 
 export function AuthLayout() {
   return(
-    <div className="grid min-h-screen grid-cols-2 max-[736px]:flex max-[736px]: flex-col">
+    <div className="grid min-h-screen grid-cols-2 max-[736px]:flex max-[736px]: flex-col antialiased">
       <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between max-[736px]:bg-white">
         <div className="flex items-center gap-3 text-lg text-foreground max-[736px]:justify-center">
           <PizzaIcon className="h-5 w-5"/>
